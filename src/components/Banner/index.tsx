1dd4e0dd31fa09b6f4842a7ab86aa3687da9911e
michaelmoranis <michaelmoranesul@gmail.com>
2024-12-30 14:23:13 -0300

    feature: adicionando fundo no banner gradient

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,16 +2,16 @@ import Button from "../Button";
 
 export default function Banner() {
     return (
-        <section className="flex flex-col w-full h-full items-center justify-center md:w-4/5 gap-8 md:flex-row-reverse  from-green-500 from-10% via-emerald-500 via-30% to-sky-500 to-90%">
+        <section className="flex flex-col w-full h-full items-center justify-center md:w-4/5 gap-8 rounded-t-xl md:flex-row-reverse bg-gradient-to-r from-green-500 from-10% via-emerald-500 via-30% to-sky-500 to-90%">
             <div className="w-full h-4/5 mt-6">
                 <div className=" bg-[url('././assets/images/image-mockups.png')] bg-cover h-96">
                 </div>
             </div>
             <section className="flex flex-col items-center justify-center p-2 gap-4 w-full h-4/5">
                 <div className="mb-8">
-                    <h3 className="text-4xl text-zinc-600 text-center font-semibold">Next Generation digital banking</h3>
+                    <h3 className="text-4xl text-white text-center font-semibold">Next Generation digital banking</h3>
                 </div>
-                <div className="text-zinc-600 text-center ">
+                <div className="text-white text-center text-xl">
                     Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
                 </div>
                 <Button />
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
     return (
-        <button className="m-4 p-2 rounded-full w-48 text-zinc-100 text-md font-semibold bg-gradient-to-r from-green-500 from-10% via-emerald-500 via-30% to-sky-500 to-90%">
+        <button className="m-4 p-2 rounded-full w-48 text-zinc-100 text-md font-semibold bg-emerald-800 border-2 border-sky-500">
             Request Invite
         </button>
     )
